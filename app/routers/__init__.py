3eaf1b639941cb97dbba2c155762f574a1d820bf
pranavcpatil05 <pranavcpatil05@gmail.com>
2026-09-07 14:42:32 +0530

    fix: correct package re-exports in models and routers __init__.py files

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,5 +1,5 @@
-from app.models.course import router as course_router
-from app.models.student import router as student_router
-from app.models.enrollment import router as enrollment_router
+from app.routers.course import router as course_router
+from app.routers.student import router as student_router
+from app.routers.enrollment import router as enrollment_router
 
 __all__ = ['course_router','student_router','enrollment_router']
