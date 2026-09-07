d597e653349728bd53b8f78b24980308345cdcef
pranavcpatil05 <pranavcpatil05@gmail.com>
2026-09-07 14:33:11 +0530

    refactor: export pydantic schemas in app/schemas/__init__.py

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -2,3 +2,8 @@ from app.schemas.course import CourseCreate, CourseUpdate, CourseResponse
 from app.schemas.enrollment import EnrollmentCreate,EnrollmentResponse
 from app.schemas.student import StudentCreate, StudentResponse, StudentUpdate
 
+__all__ = [
+    "CourseCreate", "CourseResponse", "CourseUpdate",
+    "StudentCreate", "StudentResponse", "StudentUpdate",
+    "EnrollmentCreate", "EnrollmentResponse"
+]
